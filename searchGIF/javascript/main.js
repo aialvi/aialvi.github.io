e88e7fe090d08417b62c93a30b0b762ac2afadf6
Aminul Islam Alvi <aminulialvi@gmail.com>
2020-01-18 18:52:09 +0600

    Added: searchGIF

---
+++ searchGIF/javascript/main.js
@@ -0,0 +1,71 @@
+/* 1. Grab the input value */
+
+
+document.querySelector(".js-go").addEventListener('click',function(){
+
+    var input = document.querySelector("input").value;
+    pushToAPI(input);
+  
+  });
+  
+  document.querySelector(".js-userinput").addEventListener('keyup',function(e){
+  
+    var input = document.querySelector("input").value;
+  
+    // if the key ENTER is pressed...
+    if(e.which === 13) {
+      pushToAPI(input);
+    }
+  
+  });
+  
+  /* 2. do the data stuff with the API */
+
+ function pushToAPI(input){
+
+    var url = "http://api.giphy.com/v1/gifs/search?q=" +input+ "&api_key=dc6zaTOxFJmzC"
+    console.log(url);
+  
+    // AJAX Request
+    var GiphyAJAXCall = new XMLHttpRequest();
+    GiphyAJAXCall.open( 'GET', url );
+    GiphyAJAXCall.send();
+    
+    GiphyAJAXCall.addEventListener('load',function(e){
+    
+      var data = e.target.response;
+      pushToDOM(data);
+    
+    });
+
+ }
+    
+    
+   
+  
+
+  
+  
+  
+  /* 3. Show me the GIFs */
+  
+  
+  function pushToDOM(input) {
+
+    var container = document.querySelector(".js-container");
+  
+    var response = JSON.parse(input);
+  
+    var imageUrls = response.data;
+  
+    imageUrls.forEach(function(image){
+  
+      var src = image.images.fixed_height.url;
+      console.log(src);
+  
+     
+      container.innerHTML += "<img src=\"" + src + "\" class=\"container-image\">";
+  
+    });
+  
+  }
