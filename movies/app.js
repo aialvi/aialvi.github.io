301602c299de1f50bd52f53c43edc62d8b83f218
Aminul Islam Alvi <aminulialvi@gmail.com>
2021-06-15 01:32:41 +0600

    modify movies

--- movies/app.js
+++ movies/app.js
@@ -11,20 +11,11 @@ new Vue({
     },
   },
 
-  methods: {
-    callApi: function () {
-      console.log("b");
-      fetch(`http://www.omdbapi.com/?t=${this.movieName}&apikey=f8db8c24`)
-        .then((response) => response.json())
-        .then((data) => {
-          this.movie = data;
-        });
-    },
-  },
+  methods: {},
 
   watch: {
     movieName: function (val) {
-      fetch(`http://www.omdbapi.com/?t=${val}&apikey=f8db8c24`)
+      fetch(`https://www.omdbapi.com/?t=${val}&apikey=f8db8c24`)
         .then((response) => response.json())
         .then((data) => {
           this.movie = data;
